cf34122e364a4a87c7137fff440d441ae832bcbd
keell0renz <sidi0us.mail@gmail.com>
2023-06-26 08:49:16 +0300

    Added database logic and started doing db schema.

---
+++ src/database.py
@@ -0,0 +1,45 @@
+from sqlalchemy import create_engine, inspect
+from sqlalchemy.orm import sessionmaker
+
+from .db_schema import Base
+
+import os
+
+def _connect(default_db_string):
+    db_string = os.getenv("SHORTLINK_DB_STRING")
+
+    if db_string:
+        engine = create_engine(db_string)
+
+    else:
+        engine = create_engine(default_db_string)
+
+def _ensure_schema_exists(session, engine, base, check_for):
+    inspector = inspect(session.bind)
+    tables = inspector.get_table_names()
+
+    if not check_for in tables:
+        base.metadata.create_all(bind=engine)
+
+    session.close()
+
+def _create_get_session(session_class):
+    def get_session():
+        session = session_class()
+
+        try:
+            yield session
+
+        finally:
+            session.close()
+
+    return get_session
+
+
+engine = _connect("postgresql://admin:secret@localhost:5678/admin")
+
+Session = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+
+_ensure_schema_exists(Session(), engine, Base, "links")
+
+get_session = _create_get_session()
---
+++ src/db_schema.py
@@ -0,0 +1,5 @@
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy import Integer, String, DateTime
+
+Base = declarative_base()
+
