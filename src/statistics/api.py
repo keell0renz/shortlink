ddd5f09fdd0de70005f64183f5afa4177bf0bb97
keell0renz <sidi0us.mail@gmail.com>
2023-06-26 11:18:48 +0300

    Added interface for api.py in statistics

---
+++ src/statistics/api.py
@@ -0,0 +1,25 @@
+from fastapi import APIRouter, Path
+
+router = APIRouter()
+
+@router.get("/link/{link_id}")
+async def stat_api_get_link_statistics(link_id: str = Path(...)):
+    pass
+
+@router.get("/links")
+async def stat_api_get_all_links():
+    pass
+
+
+@router.get("/active_links")
+async def stat_api_get_active_links():
+    pass
+
+
+@router.get("/expired_links")
+async def stat_api_get_expired_links():
+    pass
+
+@router.get("/interactions/{link_id}")
+async def stat_api_get_link_interactions(link_id: str = Path(...)):
+    pass
