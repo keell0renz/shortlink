84c9c324f55cafbcc80f8c4a2d44fdecd5a369a9
keell0renz <sidi0us.mail@gmail.com>
2023-06-26 11:35:12 +0300

    Saved changes

---
+++ src/exceptions.py
@@ -0,0 +1,5 @@
+from fastapi import FastAPI
+
+app = FastAPI()
+
+# Here goes exception handling
---
+++ src/main.py
@@ -0,0 +1,9 @@
+from .exceptions import app
+
+from .interface import ui as ui
+from .interface import api as api
+from .statistics import api as stat_api
+
+app.include_router(ui.router, prefix="", tags=["User Interface"])
+app.include_router(api.router, prefix="/api", tags=["API"])
+app.include_router(stat_api.router, prefix="/statistics", tags=["Statistics API"])
