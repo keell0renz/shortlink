41ebe08c837fd5e76b1507d457937c15f1c60eb1
keell0renz <sidi0us.mail@gmail.com>
2023-06-28 11:02:40 +0300

    Reduced redundancy in statistics/api.py by introducing dependency injection

--- src/statistics/api.py
+++ src/statistics/api.py
@@ -1,11 +1,11 @@
 from fastapi import APIRouter, Path, Depends
 
-from ..utils import verify_api_key
+from ..utils import verify_api_key, get_link_id
 
 router = APIRouter(dependencies=[Depends(verify_api_key)])
 
 @router.get("/link/{link_id}")
-async def stat_api_get_link_statistics(link_id: str = Path(...)):
+async def stat_api_get_link_statistics(link_id: str = Depends(get_link_id)):
     pass
 
 @router.get("/links")
@@ -23,5 +23,5 @@ async def stat_api_get_expired_links():
     pass
 
 @router.get("/interactions/{link_id}")
-async def stat_api_get_link_interactions(link_id: str = Path(...)):
+async def stat_api_get_link_interactions(link_id: str = Depends(get_link_id)):
     pass
