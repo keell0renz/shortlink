2c164ebdfc78e2b8e05aa1b4f5b92c23ee5f1cfd
keell0renz <sidi0us.mail@gmail.com>
2023-06-29 16:14:04 +0300

    Added LinkRepository implementation

--- src/interface/logic.py
+++ src/interface/logic.py
@@ -7,6 +7,8 @@ from sqlalchemy.orm import Session
 from sqlalchemy.exc import SQLAlchemyError, IntegrityError
 from ..database.db_schema import Link, Interactions
 
+from ..exceptions import LinkAlreadyExists, LinkDoesNotExist, InternalSQLAlchemyError
+
 class LinkDatabaseRecord(BaseModel):
     link_id: str
     original_link: str
@@ -42,7 +44,81 @@ class LinkRepository(AbstractLinkRepository):
     def __init__(self, session: Session):
         self.session = session
 
-    #TODO Repository!
+    def create_link(self, link_id: str, original_link: str) -> None:
+        try:
+            link = Link(link_id=link_id, original_link=original_link)
+            self.session.add(link)
+            self.session.commit()
+
+        except IntegrityError:
+            self.session.rollback()
+            raise LinkAlreadyExists(link_id)
+        
+        except SQLAlchemyError as e:
+            self.session.rollback()
+            raise InternalSQLAlchemyError(str(e))
+
+    def get_link_by_link_id(self, link_id: str) -> LinkDatabaseRecord:
+        link = self.session.query(Link).filter_by(link_id=link_id).first()
+
+        if link is None:
+            raise LinkDoesNotExist(link_id)
+        
+        return LinkDatabaseRecord(**link.__dict__)
+
+    def delete_link_by_link_id(self, link_id: str) -> None:
+        try:
+            link = self.session.query(Link).filter_by(link_id=link_id).first()
+            if link is None:
+                raise LinkDoesNotExist(link_id)
+            
+            self.session.delete(link)
+            self.session.commit()
+
+        except SQLAlchemyError as e:
+            self.session.rollback()
+            raise InternalSQLAlchemyError(str(e))
+
+    def change_original_link(self, link_id: str, new_original_link: str) -> None:
+        try:
+            link = self.session.query(Link).filter_by(link_id=link_id).first()
+            if link is None:
+                raise LinkDoesNotExist(link_id)
+            
+            link.original_link = new_original_link
+            self.session.commit()
+
+        except IntegrityError:
+            self.session.rollback()
+            raise LinkAlreadyExists(link_id)
+        
+        except SQLAlchemyError as e:
+            self.session.rollback()
+            raise InternalSQLAlchemyError(str(e))
+
+    def change_expiration_time(self, link_id: str, new_expiration_time: datetime) -> None:
+        try:
+            link = self.session.query(Link).filter_by(link_id=link_id).first()
+            if link is None:
+                raise LinkDoesNotExist(link_id)
+            
+            link.expiration_time = new_expiration_time
+            self.session.commit()
+
+        except SQLAlchemyError as e:
+            self.session.rollback()
+            raise InternalSQLAlchemyError(str(e))
+
+    def add_interaction_by_link(self, link_id: str, ip: str, country: str, user_agent: str) -> None:
+        try:
+            interaction = Interactions(link_id=link_id, ip=ip, country=country, user_agent=user_agent)
+            self.session.add(interaction)
+            self.session.commit()
+            
+        except SQLAlchemyError as e:
+            self.session.rollback()
+            raise InternalSQLAlchemyError(str(e))
+
 
 class LinkInterface():
     def __init__(self, repository: AbstractLinkRepository):
