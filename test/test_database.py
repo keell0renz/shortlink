1511a0093b8190cc7112d9c86e24d9c01ae33a60
keell0renz <sidi0us.mail@gmail.com>
2023-07-02 16:53:48 +0300

    PEP8 Compliance

--- test/test_database.py
+++ test/test_database.py
@@ -5,12 +5,5 @@ from src.database.database import get_session
 
 # No tests temporarily!
 
-
-class TestDatabase(unittest.TestCase):
-    def test_getting_session(self):
-        with get_session() as session:
-            pass
-
-
 if __name__ == "__main__":
     unittest.main()
