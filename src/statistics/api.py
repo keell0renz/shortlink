2d097161ca06897b31739c22580383adfde3844c
keell0renz <sidi0us.mail@gmail.com>
2023-07-02 16:18:06 +0300

    Removed not useful method in statistics

--- src/statistics/api.py
+++ src/statistics/api.py
@@ -10,19 +10,13 @@ async def get_interactions_repository(session: Session = Depends(get_session)):
 
 router = APIRouter(dependencies=[Depends(verify_api_key)])
 
-@router.get("/link/{link_id}")
-async def stat_api_get_link_statistics(link_id: str = Depends(get_link_id), repository: InteractionsRepository = Depends(get_interactions_repository)):
-    pass
-
 @router.get("/links")
 async def stat_api_get_all_links(repository: InteractionsRepository = Depends(get_interactions_repository)):
     pass
 
-
 @router.get("/active_links")
 async def stat_api_get_active_links(repository: InteractionsRepository = Depends(get_interactions_repository)):
     pass
-
 
 @router.get("/expired_links")
 async def stat_api_get_expired_links(repository: InteractionsRepository = Depends(get_interactions_repository)):
