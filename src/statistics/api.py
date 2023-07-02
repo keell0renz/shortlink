3117b93f55bfdf5aa94b6b86a2fda56802d576bd
keell0renz <sidi0us.mail@gmail.com>
2023-07-02 16:20:03 +0300

    Added functiinality to statistics api

--- src/statistics/api.py
+++ src/statistics/api.py
@@ -1,4 +1,4 @@
-from fastapi import APIRouter, Path, Depends
+from fastapi import APIRouter, Path, Depends, status
 from ..utils import verify_api_key, get_link_id
 
 from sqlalchemy.orm import Session
@@ -10,18 +10,18 @@ async def get_interactions_repository(session: Session = Depends(get_session)):
 
 router = APIRouter(dependencies=[Depends(verify_api_key)])
 
-@router.get("/links")
+@router.get("/links", status_code=status.HTTP_200_OK)
 async def stat_api_get_all_links(repository: InteractionsRepository = Depends(get_interactions_repository)):
-    pass
+    return repository.get_all_active_links()
 
-@router.get("/active_links")
+@router.get("/active_links", status_code=status.HTTP_200_OK)
 async def stat_api_get_active_links(repository: InteractionsRepository = Depends(get_interactions_repository)):
-    pass
+    return repository.get_all_active_links()
 
-@router.get("/expired_links")
+@router.get("/expired_links", status_code=status.HTTP_200_OK)
 async def stat_api_get_expired_links(repository: InteractionsRepository = Depends(get_interactions_repository)):
-    pass
+    return repository.get_all_expired_links()
 
-@router.get("/interactions/{link_id}")
+@router.get("/interactions/{link_id}", status_code=status.HTTP_200_OK)
 async def stat_api_get_link_interactions(link_id: str = Depends(get_link_id), repository: InteractionsRepository = Depends(get_interactions_repository)):
-    pass
+    return repository.get_link_interactions_by_link_id(link_id)
