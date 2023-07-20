d72504066fb1795313decda8be9ad7cd15797e33
Bohdan Agarkov <sidi0us.mail@gmail.com>
2023-07-20 15:46:38 +0300

    Added files for pytest.

--- test/test_database.py
+++
@@ -1,9 +0,0 @@
-import unittest
-from src.database.database import get_session
-
-# python3 -m unittest discover -p "test*.py" -s test
-
-# No tests temporarily!
-
-if __name__ == "__main__":
-    unittest.main()
