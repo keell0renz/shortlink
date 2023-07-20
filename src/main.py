6cf1e989d37d3a252e84491d8453b7be2dee1307
Bohdan Agarkov <sidi0us.mail@gmail.com>
2023-07-20 16:27:43 +0300

    Started doing API v2

--- src/main.py
+++ src/main.py
@@ -1,10 +1,10 @@
 from .exceptions import app
 from .interface import ui as ui
-from .interface import api as api
+from .interface import api_v1 as api_v1
 from .statistics import api as stat_api
 
 app.include_router(ui.router, prefix="", tags=["User Interface"])
-app.include_router(api.router, prefix="/api", tags=["API"])
+app.include_router(api_v1.router, prefix="/api/v1", tags=["API"])
 app.include_router(
     stat_api.router,
     prefix="/statistics",
