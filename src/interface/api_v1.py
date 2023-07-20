5188f54efa412ceb0583697dff78635b67c8bd2d
Bohdan Agarkov <sidi0us.mail@gmail.com>
2023-07-20 17:13:25 +0300

    Added depreciation warning for API v1

--- src/interface/api_v1.py
+++ src/interface/api_v1.py
@@ -13,7 +13,7 @@ from ..utils import (
 
 router = APIRouter(dependencies=[Depends(verify_api_key)])
 
-@router.get("/get/{link_id}", status_code=status.HTTP_200_OK)
+@router.get("/get/{link_id}", status_code=status.HTTP_200_OK, summary="(Deprecated)")
 def api_get_link(
     link_id: str = Depends(get_link_id),
     interface: LinkInterface = Depends(get_link_interface)
@@ -22,7 +22,7 @@ def api_get_link(
     
     return link_dict
 
-@router.post("/create/{link_id}", status_code=status.HTTP_201_CREATED)
+@router.post("/create/{link_id}", status_code=status.HTTP_201_CREATED, summary="(Deprecated)")
 def api_create_link(
     link_id: str = Depends(get_link_id),
     original_link: str = Depends(get_original_link),
@@ -31,14 +31,14 @@ def api_create_link(
 ):
     interface.create_link(link_id, original_link, expiration_time)
 
-@router.delete("/delete/{link_id}", status_code=status.HTTP_201_CREATED)
+@router.delete("/delete/{link_id}", status_code=status.HTTP_201_CREATED, summary="(Deprecated)")
 def api_delete_link(
     link_id: str = Depends(get_link_id),
     interface: LinkInterface = Depends(get_link_interface)
 ):
     interface.delete_link(link_id)
 
-@router.put("/change/{link_id}/original_link", status_code=status.HTTP_201_CREATED)
+@router.put("/change/{link_id}/original_link", status_code=status.HTTP_201_CREATED, summary="(Deprecated)")
 def api_change_original_link(
     link_id: str = Depends(get_link_id),
     new_original_link: str = Depends(get_original_link),
@@ -46,7 +46,7 @@ def api_change_original_link(
 ):
     interface.change_original_link_by_link_id(link_id, new_original_link)
 
-@router.put("/change/{link_id}/expiration_time", status_code=status.HTTP_201_CREATED)
+@router.put("/change/{link_id}/expiration_time", status_code=status.HTTP_201_CREATED, summary="(Deprecated)")
 def api_change_expiration_time(
     link_id: str = Depends(get_link_id),
     new_expiration_time: Union[str, None] = Depends(get_expiration_time),
@@ -54,7 +54,7 @@ def api_change_expiration_time(
 ):
     interface.change_expiration_time_by_link_id(link_id, new_expiration_time)
 
-@router.delete("/delete/{link_id}/expiration_time", status_code=status.HTTP_201_CREATED)
+@router.delete("/delete/{link_id}/expiration_time", status_code=status.HTTP_201_CREATED, summary="(Deprecated)")
 def api_delete_expiration_time(
     link_id: str = Depends(get_link_id),
     interface: LinkInterface = Depends(get_link_interface)
